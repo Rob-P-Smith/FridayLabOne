908a59d3a3c130245543515af5a62c203f19b335
Clav <smith.rob@student.greenriver.edu>
2024-09-26 15:53:31 -0700

    imported packages to main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import lodash from 'lodash';
 import chalk from 'chalk';
+import colors from 'colors';
+import { faker } from '@faker-js/faker';
+import validator from 'validator';
 
 const array = [1,2,3,4,5,6,6,2,1,2,3,4,89];
 const uniqueNums = lodash.uniq(array);
