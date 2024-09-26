df4e7bb3e1d0345a7d05c8cd859f5ce0a5936fd3
Clav <smith.rob@student.greenriver.edu>
2024-09-26 16:01:19 -0700

    Testing Nodemon function

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ import validator from 'validator';
 
 const array = [1,2,3,4,5,6,6,2,1,2,3,4,89];
 const uniqueNums = lodash.uniq(array);
-console.log(chalk.yellow(uniqueNums));
+console.log(uniqueNums);
+console.log(chalk.yellow.bgRed(uniqueNums));
 
-console.log(chalk.blueBright("Hello, with chalk!"));
+console.log(chalk.blueBright.bgCyan("Hello, with chalk and colors!"));
 
 
